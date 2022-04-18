d80270f9fd47e1b70deb32f0147d58e2ace23117
Ethan <elin@cs.fsu.edu>
2022-04-18 10:12:19 -0400

    Testing key.

--- testing.cc
+++ testing.cc
@@ -3,11 +3,13 @@
 using namespace std;
 
 int main(){
+  double * happy = new double[1000];
   char * shape = new char[10];
   int x = 0;
   cout << "Enter a number: ";
   cin >> x;
   cout << "Your number is: " << x << endl;
   delete [] shape;
+  delete[] happy;
   cout << "Deleted shape\n";
 }
