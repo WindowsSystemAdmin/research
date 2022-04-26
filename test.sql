7f38cd9faa393b0d4b541f2fbfe9cf5e3102c700
WindowsSystemAdmin <linethan0322@gmail.com>
2022-04-26 19:30:36 -0400

    Create test.sql

---
+++ test.sql
@@ -0,0 +1,9 @@
+Begin Tran  
+Insert INTO tblStudents VALUES ('Kajal')  
+SAVE Tran A;  
+Insert INTO tblStudents VALUES ('Rahul')  
+SAVE Tran B;  
+Insert INTO tblStudents VALUES ('Ram')  
+SAVE Tran C;  
+  
+SELECT * from tblStudents 
